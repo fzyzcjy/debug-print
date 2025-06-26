0e87bc58a771c3a24fcc4f28353ddf3e19be6be0
fzyzcjy <ch271828n@outlook.com>
2025-06-26 16:30:12 +0800

    more

--- debug_print/__init__.py
+++ debug_print/__init__.py
@@ -1,5 +1,30 @@
+from typing import Dict
+
 import torch
 from ._kernels import print_tensor as print_tensor_kernel
+
+
+class _Buffer:
+    def __init__(self, device_index: int):
+        self._tensor = torch.zeros((10_000_000,), dtype=torch.char, device=f"cuda:{device_index}")
+        self._used_len = 0
+
+    def allocate(self, size: int):
+        output = self._tensor[self._used_len: self._used_len + size]
+        self._used_len += size
+        assert self._used_len <= len(self._tensor)
+        return output
+
+
+class _DebugPrinter:
+    def __init__(self):
+        # Can be optimized
+        self._buffers: Dict[int, _Buffer] = {
+            device_index: _Buffer(device_index=device_index)
+            for device_index in range(torch.cuda.device_count())
+        }
+
+    def __call__(self, x: torch.Tensor, name: str = "", print_ptr: bool = False):
 
 
 def print_tensor(x: torch.Tensor, print_ptr: bool = False):
